717cefe84944a9d23a2c4e1613d4b11c03e97c8a
Tony <toneyobaze@gmail.com>
2022-10-20 16:40:56 +0000

    create mainheader component

---
+++ src/components/mainHeader/MainHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./mainHeader.css";
+
+const MainHeader = () => {
+  return (
+    <div className="row header-layout">
+      <h4>SPLI</h4>
+      <h4>TTER</h4>
+    </div>
+  );
+};
+
+export default MainHeader;
