8096272c7f40384f8c0354f44579808427eff65b
Tony <toneyobaze@gmail.com>
2022-10-21 13:10:48 +0000

    validate form input

--- src/components/formInput/FormInput.jsx
+++ src/components/formInput/FormInput.jsx
@@ -1,39 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./formInput.css";
 
 const FormInput = ({ labelText, Icon, value, dispatch }) => {
-  const [showBorder, setShowBorder] = useState(false);
-  const [focused, setFocused] = useState(false);
-  const [borderColor, setBorderColor] = useState("hsl(189, 41%, 97%)");
-
-  const validateForm = () => {
-    if (value === 0) setBorderColor("10, 90%, 61%");
-    else setBorderColor("hsl(189, 41%, 97%)");
-  };
+  const [showError, setShowError] = useState(false);
 
   return (
-    <div style={{ margin: "1rem 0" }}>
-      <label className="input-label">{labelText}</label>
+    <div className="custom-input">
+      <div className="label-container">
+        <label className="input-label">{labelText}</label>
+        {showError && <label className="error-message">Can't be zero</label>}
+      </div>
       <div
-        className="input-group mb-3 input-container"
-        style={{
-          borderColor,
-          borderWidth: "2px",
-        }}
+        className={`input-group mb-3 input-container ${
+          showError && "error-container"
+        }`}
       >
         <div className="icon-container">
           <Icon />
         </div>
         <input
           value={value || ""}
+          onBlur={() =>
+            value === 0 ? setShowError(true) : setShowError(false)
+          }
           onChange={(e) => dispatch(e.target.value || 0)}
-          onBlur={() => {
-            setFocused(false);
-            validateForm();
-          }}
-          onFocus={() => setFocused(true)}
-          onMouseOver={() => setBorderColor("hsl(172, 67%, 45%)")}
-          onMouseOut={() => !focused && setBorderColor("hsl(189, 41%, 97%)")}
           type="number"
           className="form-control shadow-none"
           placeholder="0"
