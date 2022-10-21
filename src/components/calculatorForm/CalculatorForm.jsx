cf23d8e5ff5cf815e32506d6e522290928acce26
Tony <toneyobaze@gmail.com>
2022-10-21 11:59:03 +0000

    pass state and dispatch to forminput component

--- src/components/calculatorForm/CalculatorForm.jsx
+++ src/components/calculatorForm/CalculatorForm.jsx
@@ -1,16 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import FormInput from "../formInput/FormInput";
 import TipSelector from "../tipSelector/TipSelector";
+import { TipContext } from "../../contexts/tipContext";
 import { ReactComponent as Person } from "../../assets/img/icon-person.svg";
 import { ReactComponent as Dollar } from "../../assets/img/icon-dollar.svg";
 import "./calculatorForm.css";
 
 const CalculatorForm = () => {
+  const [state, dispatch] = useContext(TipContext);
+
   return (
     <>
-      <FormInput labelText="Bill" Icon={Dollar} />
-      <TipSelector />
-      <FormInput labelText="Number of People" Icon={Person} />
+      <FormInput
+        labelText="Bill"
+        Icon={Dollar}
+        value={state.bill}
+        dispatch={(val) => dispatch({ type: "setBill", value: parseInt(val) })}
+      />
+      <TipSelector dispatch={() => dispatch({ type: "setTipPercent" })} />
+      <FormInput
+        labelText="Number of People"
+        Icon={Person}
+        value={state.numberOfPeople}
+        dispatch={(val) =>
+          dispatch({ type: "setNumberOfPeople", value: parseInt(val) })
+        }
+      />
     </>
   );
 };
--- src/components/formInput/FormInput.jsx
+++ src/components/formInput/FormInput.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./formInput.css";
 
-const FormInput = ({ labelText, Icon }) => {
+const FormInput = ({ labelText, Icon, value, dispatch }) => {
   const [showBorder, setShowBorder] = useState(false);
   const [focused, setFocused] = useState(false);
-  const [value, setValue] = useState();
+  const [borderColor, setBorderColor] = useState("hsl(189, 41%, 97%)");
 
-  useEffect(() => {
-    setShowBorder(focused);
-  }, [focused]);
+  const validateForm = () => {
+    if (value === 0) setBorderColor("10, 90%, 61%");
+    else setBorderColor("hsl(189, 41%, 97%)");
+  };
 
   return (
     <div style={{ margin: "1rem 0" }}>
@@ -16,19 +17,23 @@ const FormInput = ({ labelText, Icon }) => {
       <div
         className="input-group mb-3 input-container"
         style={{
-          borderColor: showBorder ? "hsl(172, 67%, 45%)" : "hsl(189, 41%, 97%)",
+          borderColor,
+          borderWidth: "2px",
         }}
       >
         <div className="icon-container">
           <Icon />
         </div>
         <input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onBlur={() => setFocused(false)}
+          value={value || ""}
+          onChange={(e) => dispatch(e.target.value || 0)}
+          onBlur={() => {
+            setFocused(false);
+            validateForm();
+          }}
           onFocus={() => setFocused(true)}
-          onMouseOver={() => setShowBorder(true)}
-          onMouseOut={() => !focused && setShowBorder(false)}
+          onMouseOver={() => setBorderColor("hsl(172, 67%, 45%)")}
+          onMouseOut={() => !focused && setBorderColor("hsl(189, 41%, 97%)")}
           type="number"
           className="form-control shadow-none"
           placeholder="0"
