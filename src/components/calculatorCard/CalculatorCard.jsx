82b8098f6cb7bdde6e8df43021f61497f50b2875
Tony <toneyobaze@gmail.com>
2022-10-21 04:48:39 +0000

    create calculatorCard component

---
+++ src/components/calculatorCard/CalculatorCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./calculatorCard.css";
+
+const CalculatorCard = () => {
+  return (
+    <div className="row main-calculator-layout">
+      <div className="col-md-6 calculator-card mx-auto">
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalculatorCard;
